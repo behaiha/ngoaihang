402c32a9249f3b941fa3a3a2066f3aa6277c3ec3
Triệu Nhữ <anhntk54@gmail.com>
2014-04-25 01:33:00 +0700

    Update view_left_sidebar.php

--- protected/modules/Event/components/views/view_left_sidebar.php
+++ protected/modules/Event/components/views/view_left_sidebar.php
@@ -1,3 +1,4 @@
+<?php $srcImg = "./images/event/image.jpg"; ?>
 <div class="user-span-3 prl-span-3 prl-entry-meta">
 	<div class="prl-article-meta">
 		<span><i class="fa fa-calendar-o"></i> Nov 18th, 2013</span> 
@@ -17,21 +18,27 @@
     	</div>
     </p>
     <hr class="prl-article-divider">
-    <p class="rating-head">User rating: 3.76 (440)</p>
-    <p class="prl-post-rating prl-user-rating">
-    <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-half-o"></i>
-    </p>						
-	<hr class="prl-article-divider">
-	<ul class="prl-list prl-list-sharing">
-		<li class="header">SHARING</li>
-		<li><a href="#"><i class="fa fa-facebook-square"></i> Facebook</a></li>
-		<li><a href="#"><i class="fa fa-twitter-square"></i> Twitter</a></li>
-		<li><a href="#"><i class="fa fa-google-plus-square"></i> Google plus</a></li>
-		<li><a href="#"><i class="fa fa-pinterest-square"></i> Pinterest</a></li>
-		<li><a href="#"><i class="fa fa-linkedin-square"></i> Linkedin</a></li>
-		<li><a href="#"><i class="fa fa-envelope"></i> Mail this article</a></li>
-		<li><a href="#" onclick="window.print();" id="print-page"><i class="fa fa-print"></i> Print this article</a></li>
-	</ul>
-	<hr class="prl-article-divider">
-	<p class="prl-article-tags"><span class="prl-article-tags-header">TAGS:</span> <a href="">News</a>, <a href="">Wordpress</a>, <a href="">HTML</a>, <a href="">CSS</a>, <a href="">Web Design</a></p>
+    <p class="rating-head">Sự kiện liên quan</p>
+    <div class="event-correlative">
+    	<div class="event-corre">
+    		<a href="">
+    			<img src="<?php echo $srcImg; ?>">
+    		</a>
+    		<a href="">Trận đấu bán kết ManU và Real</a>
+    		<a href="">Tham gia</a>
+
+    	</div>
+    </div>
 </div>
+<style type="text/css">
+div.join{
+	float: left;
+	margin-left: 3px;
+	border: 1px solid #eee;
+	padding: 3px 8px 0px;
+	box-shadow: 0px 0px 3px;
+	border-radius: 7%;
+	border-color: #999 #777 #777 #999;
+	background: -webkit-gradient(linear,center top,center bottom,from(#FFFFFF),to(#D4D4C4));
+}
+</style>
